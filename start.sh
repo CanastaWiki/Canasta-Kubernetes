3daebc421ef6c2e46fdf39ec4814fd86b3bb3136
Yaron Koren <yaron57@gmail.com>
2022-09-02 16:02:41 -0400

    Typo fixes for comments in start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
-# Delete pervious configmaps
+# Delete previous ConfigMaps
 kubectl delete configmap canasta-settings
 kubectl delete configmap canasta-config
 kubectl delete configmap canasta-env
 
-# Create configmaps from the configuration files
+# Create ConfigMaps from the configuration files
 kubectl create configmap canasta-settings --from-file=./settings/
 kubectl create configmap canasta-config --from-file=./config/
 kubectl create configmap canasta-env --from-env-file=.env
 
-# Create all the kuberenets deployments, services and realates objects.
+# Create all the Kubernetes deployments, services and related objects
 kubectl apply -f Kubernetes/
